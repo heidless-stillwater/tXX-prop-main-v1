330291bb42dc74f8f18608e4e315f29280600f01
rob <heidlessemail14@gmail.com>
2025-08-10 09:26:20 +0000

    still not seeing image

--- next.config.ts
+++ next.config.ts
@@ -13,15 +13,11 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
+      },
     ],
   },
 };
